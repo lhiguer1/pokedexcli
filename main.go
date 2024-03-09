b9ac47860974c88cbea313ae569e460cc3da156a
Leonel Higuera <lhiguer1@asu.edu>
2024-03-09 16:22:49 -0700

    add mutex to cache

--- main.go
+++ main.go
@@ -10,11 +10,13 @@ type config struct {
 	pokeAPIClient       pokeapi.Client
 	nextLocationAreaURL *string
 	prevLocationAreaURL *string
+	caughtPokemon       map[string]pokeapi.Pokemon
 }
 
 func main() {
 	cfg := config{
 		pokeAPIClient: pokeapi.NewClient(time.Minute, time.Hour),
+		caughtPokemon: make(map[string]pokeapi.Pokemon),
 	}
 	startRepl(&cfg)
 }
